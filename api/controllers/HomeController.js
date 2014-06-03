47d2eba26f2c8b039de52cf9fb3ceb8d18d81dda
unknown <superneohuang@gmail.com>
2014-06-03 19:09:28 +0200

    add a new layout for dashboard

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -3,7 +3,8 @@ module.exports = {
 	index: function(req, res){
 		sails.log.info("getsession" + JSON.stringify(req.session.user));
 		if (req.session.user){
-			res.view('home/dashboard');
+			var username = req.session.user.firstName + " " + req.session.user.lastName;
+			res.view('home/dashboard', {layout: "dashboardLayout", username: username});
 		}
 		else {
 			res.redirect('/login');
