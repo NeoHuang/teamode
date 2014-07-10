cea1d9ccc95a9b733f413dfed21bd56dfd51966a
NeoHuang <superneohuang@gmail.com>
2014-07-10 23:49:34 +0200

    more validation on client side for signin

--- assets/js/signupCtrl.js
+++ assets/js/signupCtrl.js
@@ -17,26 +17,36 @@ teamodeApp.factory("signupService", ["$http", function($http){
 SignupCtrl = function($scope, $http, signupService){
 	$scope.user = {};
 	$scope.alerts=[];
+	var clearError = function(){
+		$scope.passwordValid = true;
+		$scope.passwordConfirmValid = true;
+		$scope.usernameValid = true;
+		$scope.emailValid = true;
+	}
+	clearError();
 	$scope.register = function(){
-
+		clearError();
 		$scope.alerts = [];
-		if (!$scope.user.username){
+		if (!/^[A-Za-z0-9_-]{3,20}$/.test($scope.user.usernamenew)){
 			$scope.alerts.push({type:'danger', msg: 'invalid username'});
+			$scope.usernameValid = false;
 		}
-		if (!$scope.user.email){
+		if (!validator.isEmail($scope.user.email)){
 			$scope.alerts.push({type:'danger', msg: 'invalid email'});
+			$scope.emailValid = false;
 		}
-		if (!$scope.user.password){
+		if (!/^.{6,20}$/.test($scope.user.password)){
 			$scope.alerts.push({type:'danger', msg: 'invalid password, the password length should be between 6 and 20'});
-			$('#password').addClass('error');
+			$scope.passwordValid = false;
 		}
 		if ($scope.passwordConfirm !== $scope.user.password){
 			$scope.alerts.push({type:'danger', msg: 're-typed password doesn\'t match password'});
+			$scope.passwordValid = false;
 		}
 		if ($scope.alerts.length === 0){
 			signupService.post(angular.toJson($scope.user), function(data){
 				if (data.error){
-					teamode.showMsg(data.error);
+					$scope.alerts.push({type:'danger', msg: data.message});
 				}
 				else {
 					window.location = "/";
@@ -44,6 +54,8 @@ SignupCtrl = function($scope, $http, signupService){
 				}
 
 				
+			}, function(data){
+					$scope.alerts.push({type:'danger', msg: data.message});
 			});
 		}
 	}
