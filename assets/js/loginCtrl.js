93c6391abe33a4ba4d35555e24c16032848b4d02
unknown <superneohuang@gmail.com>
2014-07-14 14:05:16 +0200

    polished login front-end logic

--- assets/js/loginCtrl.js
+++ assets/js/loginCtrl.js
@@ -1,4 +1,4 @@
-var teamodeApp = angular.module("teamodeLogin",[]);
+var teamodeApp = angular.module("teamodeLogin",['ui.bootstrap']);
 teamodeApp.factory("httpService", ["$http", teamode.httpServices]);
 
 LoginCtrl = function($scope, httpService){
@@ -7,17 +7,35 @@ LoginCtrl = function($scope, httpService){
 		password: "",
 		remember: "false"
 	}
+	$scope.passwordValid = true;
+	$scope.usernameValid = true;
 	$scope.login = function(){
-		console.log("login clicked");
+		if (!$scope.user.username){
+			$scope.alerts = [];
+			$scope.alerts.push({type:'danger', msg: 'please enter username'});
+			$scope.usernameValid = false;
+			return;
+		}
+		if (!$scope.user.password){
+			
+			$scope.alerts = [];
+			$scope.alerts.push({type:'danger', msg: 'please enter password'});
+			$scope.passwordValid = false;
+			return;
+		}
 		httpService.login($scope.user, function(data){
-			console.log(data);
 			if (data.error){
-				teamode.showMsg(data.message);
+				$scope.alerts = [];
+				$scope.alerts.push({type:'danger', msg:data.message});
 			}
 			else {
 				window.location = "/";
 			}
 		})
+	}
+
+	$scope.closeAlert = function(index){
+		$scope.alerts.splice(index, 1);
 	}
 
 }
